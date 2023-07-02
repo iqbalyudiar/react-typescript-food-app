61b285412e733f02701b6e13d8b7a178480557e1
Iqbal Yudiar <iqbal.yudiar@dana.id>
2023-07-02 15:45:06 +0700

    feat: add translation for restaurant list title

--- src/constant/translation/en_US.ts
+++ src/constant/translation/en_US.ts
@@ -3,4 +3,7 @@ export default {
   'home.title': 'Welcome To Food App',
   'home.item.restaurant': 'Go To Restaurants List',
   'home.item.food': 'Go To Foods List',
+
+  // Restaurant List Page
+  'restaurant.list.title': 'Restaurant List',
 };
--- src/constant/translation/id_ID.ts
+++ src/constant/translation/id_ID.ts
@@ -3,4 +3,7 @@ export default {
   'home.title': 'Selamat Datang di Food App',
   'home.item.to.restaurant.page': 'Menuju Daftar Restoran',
   'home.item.to.food.page': 'Menuju Dafatar Makanan',
+
+  // Restaurant List Page
+  'restaurant.list.title': 'Daftar Restoran',
 };
