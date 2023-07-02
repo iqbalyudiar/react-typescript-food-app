7d8bd644417164da96d99998b277c35144ffb4bf
Iqbal Yudiar <iqbal.yudiar@dana.id>
2023-07-02 15:46:23 +0700

    feat: add restaurant list page with mock data

--- src/pages/RestaurantListPage/RestaurantList.tsx
+++ src/pages/RestaurantListPage/RestaurantList.tsx
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { useTranslation } from 'react-i18next';
+import {
+  Avatar,
+  Box,
+  Divider,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from '@mui/material';
+import { ChevronRight } from '@mui/icons-material';
 
 const RestaurantList: React.FC = () => {
-  return <div>RestaurantList</div>;
+  const { t } = useTranslation();
+  const items = [
+    {
+      id: 1,
+      name: 'Mc Donalds',
+      logo:
+        'https://seeklogo.com/images/M/mc-donald-s-logo-8568BCE03C-seeklogo.com.png',
+    },
+    {
+      id: 2,
+      name: 'KFC',
+      logo:
+        'https://seeklogo.com/images/K/kfc-new-logo-72E6348046-seeklogo.com.png',
+    },
+  ];
+  return (
+    <Box sx={{ width: '100%', paddingY: 2 }}>
+      <Typography variant="h1" sx={{ fontSize: 24, mb: 4 }}>
+        {t('restaurant.list.title')}
+      </Typography>
+      <List>
+        {items.map((item) => {
+          return (
+            <Fragment>
+              <ListItem sx={{ width: '100%', paddingY: 2 }} key={item.id}>
+                <ListItemAvatar>
+                  <Avatar alt={item.name} src={item.logo} />
+                </ListItemAvatar>
+                <ListItemText>{item.name}</ListItemText>
+                <ListItemIcon sx={{ justifyContent: 'flex-end' }}>
+                  <ChevronRight />
+                </ListItemIcon>
+              </ListItem>
+              <Divider />
+            </Fragment>
+          );
+        })}
+      </List>
+    </Box>
+  );
 };
 
 export default RestaurantList;
