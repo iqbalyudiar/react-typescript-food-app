a36c1635550a7542304ec32f56c03a5ec3e48f88
Iqbal Yudiar <iqbal.yudiar@dana.id>
2023-07-02 14:02:59 +0700

    feat: add list item on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,46 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
+import {
+  Typography,
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+} from '@mui/material';
+import { LocalDining, Fastfood } from '@mui/icons-material';
 
 const Home: React.FC = () => {
   const { t } = useTranslation();
+
+  const items = [
+    {
+      id: 1,
+      text: t('home.item.restaurant'),
+      icon: <LocalDining />,
+    },
+    {
+      id: 2,
+      text: t('home.item.food'),
+      icon: <Fastfood />,
+    },
+  ];
   return (
-    <Fragment>
-      <p>{t('home.title')}</p>
-    </Fragment>
+    <Box sx={{ paddingX: 4, paddingY: 2 }}>
+      <Typography variant="h3" sx={{ marginBottom: 4 }}>
+        {t('home.title')}
+      </Typography>
+      <List>
+        {items.map((item) => {
+          return (
+            <ListItem key={item.id}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          );
+        })}
+      </List>
+    </Box>
   );
 };
 
