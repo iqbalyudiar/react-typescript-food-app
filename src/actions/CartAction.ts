07ca7ec3ba05e50ddca96a8f69f73a3c8f600e88
Iqbal Yudiar <iqbal.yudiar@dana.id>
2023-07-07 22:37:26 +0700

    feat: add cart action removeFromCart

--- src/actions/CartAction.ts
+++ src/actions/CartAction.ts
@@ -1,4 +1,4 @@
-import { ADD_TO_CART } from '@/constant/redux/types';
+import { ADD_TO_CART, REMOVE_FROM_CART } from '@/constant/redux/types';
 import { Dispatch } from 'redux';
 
 export const addToCart: any = (food: any, restaurant: any) => async (
@@ -7,5 +7,15 @@ export const addToCart: any = (food: any, restaurant: any) => async (
   dispatch({
     type: ADD_TO_CART,
     payload: { food, restaurant },
+  });
+};
+
+export const removeFromCart: any = (id: string | number) => async (
+  dispatch: Dispatch,
+) => {
+  const food = { id };
+  dispatch({
+    type: REMOVE_FROM_CART,
+    payload: { food },
   });
 };
