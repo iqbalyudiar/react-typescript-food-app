1125bd8c51bf586ebd3fbcbda55ae0638dd32183
Iqbal Yudiar <iqbal.yudiar@dana.id>
2023-07-07 22:22:47 +0700

    feat: add new type for cart redux

--- src/constant/redux/types.ts
+++ src/constant/redux/types.ts
@@ -1,1 +1,4 @@
 export const FETCH_RESTAURANTS = 'fetchRestaurants';
+export const ADD_TO_CART = 'addToCart';
+export const REMOVE_FROM_CART = 'removeFromCart';
+export const RESET_CART = 'resetCart';
