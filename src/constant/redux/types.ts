e001f77edb32aa8289b279483b162126ac57d263
Iqbal Yudiar <iqbal.yudiar@dana.id>
2023-07-08 22:15:51 +0700

    feat: add new type redux

--- src/constant/redux/types.ts
+++ src/constant/redux/types.ts
@@ -2,3 +2,5 @@ export const FETCH_RESTAURANTS = 'fetchRestaurants';
 export const ADD_TO_CART = 'addToCart';
 export const REMOVE_FROM_CART = 'removeFromCart';
 export const RESET_CART = 'resetCart';
+export const ADD_ORDER = 'addOrder';
+export const FETCH_ORDERS = 'getOrders';
