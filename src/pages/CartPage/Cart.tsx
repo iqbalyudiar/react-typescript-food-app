fcbf61ecda026f2e0ea29fbcbd3f07fe5c265322
Iqbal Yudiar <iqbal.yudiar@dana.id>
2023-07-08 22:16:50 +0700

    feat: add order summary and add order to cart page

--- src/pages/CartPage/Cart.tsx
+++ src/pages/CartPage/Cart.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { addOrder } from '@/actions/OrderAction';
+import OrderSummary from '@/components/OrderSummary/OrderSummary';
+import { Box } from '@mui/material';
 
-const Cart = () => {
-  return <div>Cart</div>;
+const Cart: React.FC = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { restaurantName, cartItems } = useSelector((state: any) => {
+    return {
+      restaurantName: state.cart.restaurant,
+      cartItems: state.cart.cartItems,
+    };
+  });
+  const totalPrice = cartItems.reduce(
+    (prev: any, curr: any) => prev + curr.price * curr.quantity,
+    0,
+  );
+
+  return (
+    <Box sx={{ width: '100%', paddingY: 2 }}>
+      <OrderSummary
+        data={cartItems}
+        restaurantName={restaurantName}
+        totalPrice={totalPrice}
+        orderAction={() =>
+          dispatch(
+            addOrder({ cartItems, totalPrice, restaurantName }, navigate('/')),
+          )
+        }
+      />
+    </Box>
+  );
 };
 
 export default Cart;
